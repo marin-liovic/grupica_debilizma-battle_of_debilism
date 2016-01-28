ec12efb8bc5376a1dda6bb29b8b4bf68d9438ca9
Marin Liovic <liovic.marin@gmail.com>
2016-01-28 23:47:35 +0100

    fix animations

--- app/js/message_handler.js
+++ app/js/message_handler.js
@@ -13,6 +13,17 @@ BattleOfDebilism.messageHandlers = {
   },
   position: function (message) {
     var enemy =_.find(BattleOfDebilism.enemies, {id: message.playerId});
+    var sprite = enemy.sprite;
+    var lastX = enemy.sprite.position.x;
+    var dx = Math.abs(lastX - message.x);
+    if (dx < 0.5) { //almost no movement
+      sprite.animations.stop();
+      sprite.frame = 0;
+    } else if (lastX > message.x) {
+      sprite.animations.play('left');
+    } else if (lastX < message.x) {
+      sprite.animations.play('right');
+    }
     enemy.sprite.position.x = message.x;
     enemy.sprite.position.y = message.y;
   },
